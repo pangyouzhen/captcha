95025d7fb48ecfe00e6fef10bf5116ddea90c359
pangyouzhen <pangyouzhen@live.com>
2021-04-19 13:48:14 +0800

    refactor

--- captcha/get_captcha.py
+++
@@ -1,7 +0,0 @@
-import requests
-import time
-for i in range(10693, 50000):
-    screenImg = "%s.png" % i
-    a = requests.get("http://upass.iwencai.com/captcha/buildcaptcha?type=flat&98065&0.8018520822388477")
-    with open(screenImg, "wb") as f:
-        f.write(a.content)
